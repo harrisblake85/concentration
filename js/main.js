f403a10d973e2c95f49c740c33612e3a3e313b68
Blake <harrisblake85@gmail.com>
2019-04-14 09:10:57 -0600

    fixed lowercase error

--- js/main.js
+++ js/main.js
@@ -110,7 +110,7 @@ let makeDeck = async () => {
 		let newCard = {
 			rank: card.value,
 			suit: card.suit,
-			cardImage: dir+"playing-cards-front/"+card.value+"_of_"+card.suit+".png"
+			cardImage: (dir+"playing-cards-front/"+card.value+"_of_"+card.suit+".png").toLowerCase()
 		};
 		cards.push(newCard);
 	};
